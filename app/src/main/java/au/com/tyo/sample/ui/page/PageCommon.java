5a0c1969d1ec1f889a526ba2d2aaefe28cd97e0b
e-tang <lingxiang.tang@gmail.com>
2018-08-05 17:55:48 +1000

    A fix for setting the local App controller type

--- app/src/main/java/au/com/tyo/sample/ui/page/PageCommon.java
+++ app/src/main/java/au/com/tyo/sample/ui/page/PageCommon.java
@@ -2,7 +2,7 @@ package au.com.tyo.sample.ui.page;
 
 import android.app.Activity;
 
-import au.com.tyo.app.Controller;
+import au.com.tyo.sample.Controller;
 import au.com.tyo.app.ui.page.Page;
 import au.com.tyo.sample.R;
 
@@ -10,9 +10,7 @@ import au.com.tyo.sample.R;
  * Created by Eric Tang (eric.tang@tyo.com.au) on 27/11/17.
  */
 
-public class PageCommon extends Page {
-
-    protected au.com.tyo.sample.Controller controller;
+public class PageCommon extends Page<Controller> {
 
     /**
      * @param controller
@@ -20,8 +18,6 @@ public class PageCommon extends Page {
      */
     public PageCommon(Controller controller, Activity activity) {
         super(controller, activity);
-
-        this.controller = (au.com.tyo.sample.Controller) controller;
 
         toolbarColor = activity.getResources().getColor(R.color.toolbarColor);
         titleTextColor = activity.getResources().getColor(R.color.white);
